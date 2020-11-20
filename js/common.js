96ec126101ed32f972f4e44c0a0b7a7dc6ed0a42
yeonwoo <dusdn1401@naver.com>
2020-11-20 17:32:38 +0900

    201120

--- js/common.js
+++ js/common.js
@@ -1,21 +1,27 @@
 $(function(){
 	
+	$('.icon-menu').on('click', function() {
+		$('.menu').toggleClass('expanded');
+		$('span').toggleClass('hidden');
+		$('.icon-menu , .icon').toggleClass('close');
+	});
 
-	const menu = document.querySelector('.menu-trigger');
-	const nav = document.querySelector('nav');
-	let navClose = false;
-	menu.addEventListener('click', function() {
-		menu.classList.toggle('active');
-		nav.classList.toggle('active');
-		navClose ? navClose=false : navClose=true;
-	})
 
-	window.addEventListener('click',function(e){
-		console.log(e.target.nodeName)
-		if(navClose && e.target.nodeName != 'NAV'){
-			menu.classList.toggle('active');
-			nav.classList.toggle('active');
-		}
-	},true)
+	// const menu = document.querySelector('.menu-trigger');
+	// const nav = document.querySelector('nav');
+	// let navClose = false;
+	// menu.addEventListener('click', function() {
+	// 	menu.classList.toggle('active');
+	// 	nav.classList.toggle('active');
+	// 	navClose ? navClose=false : navClose=true;
+	// })
+
+	// window.addEventListener('click',function(e){
+	// 	console.log(e.target.nodeName)
+	// 	if(navClose && e.target.nodeName != 'NAV'){
+	// 		menu.classList.toggle('active');
+	// 		nav.classList.toggle('active');
+	// 	}
+	// },true)
 
 });

c6c0b00d49e930a68c1e454953d718b194f7e7b5
yeonwoo <dusdn1401@naver.com>
2020-11-25 11:54:36 +0900

    201125

--- js/project.js
+++ js/project.js
@@ -1,67 +1,60 @@
-$(function(){
-
-	var square = document.querySelector('.square');
-	var pop = document.querySelector('.pop');
-	square.addEventListener('click', function(){
-		square.classList.toggle('active');
-		pop.classList.toggle('active');
-	})
-
-// ====================================
 
 
-	$('.fade').not(':first').hide();
-            var idx = 0;
-            var loop,workSet;
-			var fa = document.querySelectorAll('.main .fade');
-			var india = document.querySelectorAll('.indi span a');
-
-            //img change
-
-			window.addEventListener('mousewheel', function fade(p) {
-
-				clearTimeout(workSet);
-				workSet = setTimeout(function(){
-					$('.fade').eq(idx).fadeOut(1000);
-					if (p == 'next' || p == undefined) {
-						idx++
-					} else {
-						idx--;
-					}
-					update();
-					$('.fade').eq(idx).fadeIn(1000);
-
-					var pg = document.querySelector('.right .rpage');
-					pg.innerHTML = (idx+1) + '/3'
-
-					$('.indi span').eq(idx).addClass('active');
-				},100)
-			});
-
-            //idx setting
-            function update() {
-                var len = $('.fade').length;
-                if (idx >= len) idx = 0;
-                if (idx == -1) idx = len - 1;
-            }
+$(function () {
 
 
-			// ==================
 
-			var liEle = document.querySelectorAll('.main .c2 figure');
-			var winH = window.innerHeight;
-		   window.addEventListener('scroll', fadeFun);
-		   function fadeFun() {
-			   liEle.forEach(function(el, idx) {
-				   if (el.offsetTop - winH < this.scrollY) {
-					   el.classList.add('active');
-				   } else {
-					   el.classList.remove('active');
-				   }
-			   });
-		   }
-	
-	
-	
-	
-});
+
+
+
+      var square = document.querySelector('.square');
+      var pop = document.querySelector('.pop');
+      square.addEventListener('click', function () {
+        square.classList.toggle('active');
+        pop.classList.toggle('active');
+      })
+
+
+
+
+      var $cont = document.querySelector('.cont');
+      var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+      var $closeBtnsArr = [].slice.call(document.querySelectorAll('.closeBtn'));
+
+      setTimeout(function () {
+        $cont.classList.remove('s--inactive');
+      }, 200);
+
+      $elsArr.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+          if (this.classList.contains('s--active')) return;
+          $cont.classList.add('s--el-active');
+          this.classList.add('s--active');
+        });
+      });
+
+      $closeBtnsArr.forEach(function ($btn) {
+        $btn.addEventListener('click', function (e) {
+          e.stopPropagation();
+          $cont.classList.remove('s--el-active');
+          document.querySelector('.el.s--active').classList.remove('s--active');
+        });
+      });
+
+
+      var liEle = document.querySelectorAll('.dt_main .dt_m2 figure');
+      var winH = window.innerHeight;
+      window.addEventListener('scroll', fadeFun);
+      function fadeFun() {
+        liEle.forEach(function (el, idx) {
+          if (el.offsetTop - winH < this.scrollY) {
+            el.classList.add('active');
+          } else {
+            el.classList.remove('active');
+          }
+        });
+      }
+    
+
+
+    });

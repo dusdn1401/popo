9f8355fda9a376b1e690f0caa837dac8ffb83a49
yeonwoo <dusdn1401@naver.com>
2020-12-03 12:10:45 +0900

    2012034

--- js/index_dev.js
+++ js/index_dev.js
@@ -1,14 +1,29 @@
 import 'babel-polyfill';
 import common from './common.js';
+import home from './home.js';
 import about from './about.js';
 import project from './project.js';
-import home from './home.js';
+
 
 function init(){
-	common();
-	home();
-	about();
-	project();
+	
+	var pageUrl = location.pathname,    //popo/index.html
+		pageS = pageUrl.lastIndexOf('/')+1,  //뒤에서 / 를 찾아 위치값을 반환
+		pageE = pageUrl.lastIndexOf('.'),    //뒤에서 . 을 찾아 위치값을 반화
+		pageName = pageUrl.slice(pageS,pageE); // 문자를 잘라서 반환 /에서 .사이의 문자 반환
+	
+	common(); //모든 페이지 호출
+	switch(pageName){
+		case 'index':home();break;
+		case 'about':about();break;
+		case 'project':project();break;
+		case 'contact':break;
+		default: home(); break;
+	}
+
+	
+	
 };
 
 window.addEventListener('DOMContentLoaded', init);
+
--- js/project.js
+++ js/project.js
@@ -1,51 +1,51 @@
 function project() {
 
-      var square = document.querySelector('.square');
-      var pop = document.querySelector('.pop');
-      square.addEventListener('click', function () {
-        square.classList.toggle('active');
-        pop.classList.toggle('active');
-      })
+  // var square = document.querySelector('.square');
+  // var pop = document.querySelector('.pop');
+  // square.addEventListener('click', function () {
+  //   square.classList.toggle('active');
+  //   pop.classList.toggle('active');
+  // })
 
-      var cont = document.querySelector('.cont');
-      var elsArr = document.querySelectorAll('.el');
-      var closeBtnsArr =document.querySelectorAll('.closeBtn');
+  var cont = document.querySelector('.cont');
+  var elsArr = document.querySelectorAll('.el');
+  var closeBtnsArr = document.querySelectorAll('.closeBtn');
 
-      setTimeout(function () {
-        cont.classList.remove('s--inactive');
-      }, 200);
+  setTimeout(function () {
+    cont.classList.remove('s--inactive');
+  }, 500);
 
-      elsArr.forEach(function (el) {
-        el.addEventListener('click', function () {
-          if (this.classList.contains('s--active')) return;
-          cont.classList.add('s--el-active');
-          this.classList.add('s--active');
-        });
-      });
+  elsArr.forEach(function (el) {
+    el.addEventListener('click', function () {
+      if (this.classList.contains('s--active')) return;
+      cont.classList.add('s--el-active');
+      this.classList.add('s--active');
+    });
+  });
 
-      closeBtnsArr.forEach(function (btn) {
-        btn.addEventListener('click', function (e) {
-          e.stopPropagation();
-          cont.classList.remove('s--el-active');
-          document.querySelector('.el.s--active').classList.remove('s--active');
-        });
-      });
+  closeBtnsArr.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      e.stopPropagation();
+      cont.classList.remove('s--el-active');
+      document.querySelector('.el.s--active').classList.remove('s--active');
+    });
+  });
 
 
-      var liEle = document.querySelectorAll('.dt_main .dt_m2 figure');
-      var winH = window.innerHeight;
-      window.addEventListener('scroll', fadeFun);
-      function fadeFun() {
-        liEle.forEach(function (el, idx) {
-          if (el.offsetTop - winH < this.scrollY) {
-            el.classList.add('active');
-          } else {
-            el.classList.remove('active');
-          }
-        });
+  var liEle = document.querySelectorAll('.dt_main .dt_m2 figure');
+  var winH = window.innerHeight;
+  window.addEventListener('scroll', fadeFun);
+  function fadeFun() {
+    liEle.forEach(function (el, idx) {
+      if (el.offsetTop - winH < this.scrollY) {
+        el.classList.add('active');
+      } else {
+        el.classList.remove('active');
       }
-    
+    });
+  }
 
 
-    };
+  console.log('project')
+}
 export default project;
